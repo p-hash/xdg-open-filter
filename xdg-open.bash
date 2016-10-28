5350380f37609adc29636f532501d3c4f600bbaa
nibogd <nibogd@users.noreply.github.com>
2016-10-29 00:07:36 +0400

    Add check for presence of config, fix check for XDG and exit check

--- xdg-open.bash
+++ xdg-open.bash
@@ -2,18 +2,23 @@
 
 XDG_OPEN="/usr/bin/xdg-open"
 
-if [[ $XDG_CONFIG_HOME ]]; then
+if [[ ! -z "$XDG_CONFIG_HOME" ]]; then
     rule_path=$XDG_CONFIG_HOME"/xdg-rules"
 else
     rule_path=$HOME"/.config/xdg-rules"
 fi
-source "$rule_path"
+if [[ -e "$rule_path" ]]; then
+    source "$rule_path"
+else
+    echo "No rules file found, please create one at $rule_path (see README.md)."
+    exit 1
+fi
 
 for regex in "${!XDG_RULES[@]}"; do
     if [[ "$1" =~ $regex ]]; then
         cmd="${XDG_RULES[$regex]}"
         eval "$cmd $1"
-        exit 1
+        [[ "$?" = "0" ]] && exit $?
     fi;
 done
 
